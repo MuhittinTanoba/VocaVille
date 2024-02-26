2cd6fba1e1516d32acb46f0f2eb14fc436d1c9e0
Muhittin Tanoba <muhitan2002@gmail.com>
2024-02-26 13:02:34 +0300

    Theme colors

--- app/src/main/java/com/example/vocaville/MainActivity.kt
+++ app/src/main/java/com/example/vocaville/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.vocaville.ui.theme.VocaVilleTheme
+import com.example.compose.VocaVilleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VocaVilleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
