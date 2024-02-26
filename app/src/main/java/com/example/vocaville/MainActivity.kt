9c6d381b471d5dc758f876c6e4452b93e9502787
Muhittin Tanoba <muhitan2002@gmail.com>
2024-02-26 13:49:53 +0300

    Header

--- app/src/main/java/com/example/vocaville/MainActivity.kt
+++ app/src/main/java/com/example/vocaville/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.VocaVilleTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,10 +28,7 @@ class MainActivity : ComponentActivity() {
             VocaVilleTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Header(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -32,17 +36,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VocaVilleTheme {
-        Greeting("Android")
+fun Header(modifier: Modifier) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            modifier = Modifier.padding(100.dp),
+            text = "VocaVille"
+        )
     }
 }
