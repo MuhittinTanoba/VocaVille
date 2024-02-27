a5f030e18288be0409e0fd72b71018843078b3cc
Muhittin Tanoba <muhitan2002@gmail.com>
2024-02-27 14:36:57 +0300

    Start screen done

--- app/src/main/java/com/example/vocaville/MainActivity.kt
+++ app/src/main/java/com/example/vocaville/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,7 +18,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose.VocaVilleTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +33,36 @@ class MainActivity : ComponentActivity() {
             VocaVilleTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Header(modifier = Modifier.padding(innerPadding))
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(modifier: Modifier){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 80.dp),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Column(modifier) {
+            Header(modifier = modifier)
+
+            Column(modifier = modifier
+                .padding(top = 100.dp)
+                .align(
+                    Alignment.CenterHorizontally
+                )
+            ) {
+                StartButton(
+                    onClick = { /*TODO*/ },
+                    modifier = modifier.align(Alignment.CenterHorizontally)
+                )
+                SettingButton(onClick = { /*TODO*/ })
             }
         }
     }
@@ -37,13 +70,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Header(modifier: Modifier) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Text(
-            modifier = Modifier.padding(100.dp),
-            text = "VocaVille"
-        )
+    Text(
+        fontSize = 40.sp,
+        text = "VocaVille"
+    )
+}
+
+@Composable
+fun StartButton(onClick: () -> Unit, modifier: Modifier){
+    Button(onClick = { onClick() }, modifier = modifier){
+        Text(text = "Start")
     }
 }
+
+@Composable
+fun SettingButton(onClick: () -> Unit) {
+    Button(onClick = { onClick() }) {
+        Text(text = "Setting")
+    }
+}
+
