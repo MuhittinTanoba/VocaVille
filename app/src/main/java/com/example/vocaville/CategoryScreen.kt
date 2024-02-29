0b64d3c472301b83bfb2cbd41c5dc1a1406df360
Muhittin Tanoba <muhitan2002@gmail.com>
2024-02-29 18:30:59 +0300

    BottomNavigationBar

--- app/src/main/java/com/example/vocaville/CategoryScreen.kt
+++ app/src/main/java/com/example/vocaville/CategoryScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,10 +14,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,36 +36,42 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun CategoryScreen(modifier: Modifier){
-    CategoryList()
+fun CategoryScreen(modifier: Modifier) {
+    Scaffold(
+        bottomBar = { BottomNavigation() }
+    ) { padding ->
+        CategoryList(padding)
+    }
 }
 
 @Composable
 @Preview
-fun CategoryCardsCollection(){
-    Surface (
+fun CategoryCardsCollection() {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
         modifier = Modifier
-            .padding(horizontal = 30.dp, vertical = 10.dp)
+            .padding(horizontal = 10.dp, vertical = 10.dp)
             .border(
                 width = 1.dp,
                 color = MaterialTheme.colorScheme.error
             )
-    ){
-        Box{
+    ) {
+        Box {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center,
-                modifier = Modifier.size(120.dp)
+                modifier = Modifier.size(140.dp)
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.category),
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
-                    modifier = Modifier.size(50.dp)
+                    modifier = Modifier.size(30.dp)
                 )
                 Text(
                     text = "Category Name",
-                    modifier = Modifier.padding(top = 10.dp)
+                    modifier = Modifier.padding(top = 10.dp),
+                    fontSize = 12.sp
                 )
             }
         }
@@ -68,16 +81,23 @@ fun CategoryCardsCollection(){
 
 
 @Composable
-@Preview
-fun CategoryList(){
+fun CategoryList(paddingValues: PaddingValues) {
     Box(
-      modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxWidth()
+            .padding(paddingValues)
     ) {
-        Column (
+        Column(
             modifier = Modifier
                 .align(Alignment.Center)
                 .fillMaxWidth()
-        ){
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                CategoryCardsCollection()
+                CategoryCardsCollection()
+            }
             Row(
                 horizontalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxWidth()
@@ -87,4 +107,46 @@ fun CategoryList(){
             }
         }
     }
+}
+
+
+@Composable
+private fun BottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "Home")
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(text = "Profile")
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BottomNavigationPreview() {
+    BottomNavigation()
 }
