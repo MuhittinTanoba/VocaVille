57b5fdca88aa18eb7426f40ea6e3df5152592508
Muhittin Tanoba <muhitan2002@gmail.com>
2024-03-04 14:12:13 +0300

    Quiz Screen Created

---
+++ app/src/main/java/com/example/vocaville/QuizScreen.kt
@@ -0,0 +1,14 @@
+package com.example.vocaville
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun QuizScreen(modifier: Modifier)
+{
+    Text(
+        text = "Quiz Screen",
+        modifier = modifier
+    )
+}
